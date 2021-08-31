6f5dadb278ec3015cd8b35237984cbb123a714e8
fobadara <62938735+fobadara@users.noreply.github.com>
2021-08-31 09:54:21 +0100

    Fix linter errors

--- javascript/index.js
+++ javascript/index.js
@@ -2,16 +2,21 @@ const hamburger = document.querySelector('.hamburger');
 const menu = document.querySelector('.nav-list-cont');
 const body = document.querySelector('body');
 const closeModal = document.querySelector('.close-modal');
-hamburger.addEventListener('click', openMenu);
-closeModal.addEventListener('click', closeMenu);
-// let sections= queryselector
+const navList = document.querySelectorAll('.nav-list');
+
 function openMenu() {
-    if (menu.classList.toggle("hamburger-toggle", true)) {
-        body.style.overflow = "hidden";
-    }
+  if (menu.classList.toggle('hamburger-toggle', true)) {
+    body.style.overflow = 'hidden';
+  }
 }
+hamburger.addEventListener('click', openMenu);
+
 function closeMenu() {
-    menu.classList.remove("hamburger-toggle");
+  menu.classList.toggle('hamburger-toggle', false);
+  body.style.overflow = 'visible';
 }
+closeModal.addEventListener('click', closeMenu);
 
-
+for (let i = 0; i < navList.length; i += 1) {
+  navList[i].addEventListener('click', closeMenu);
+}
